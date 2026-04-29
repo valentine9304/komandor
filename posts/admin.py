08621cf6f6f05875c1578eee50fd202ca452b66f
Валентин Школьный <valentine9304@gmail.com>
2026-04-29 14:16:27 +0700

    Add Post and Like Admin Models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Like, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("title", "content", "author__username")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "post", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "post__title")
+    readonly_fields = ("created_at",)
