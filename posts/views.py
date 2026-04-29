5cfaf24cc77bc33dd434d7c3e88c4ce7b1368b9e
Валентин Школьный <valentine9304@gmail.com>
2026-04-29 15:01:54 +0700

    Add METHODS Description

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,43 @@
 from django.db.models import Count
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import permissions, viewsets
 
 from posts.models import Post
 from posts.permissions import IsAuthorOrReadOnly
 from posts.serializers import PostSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(
+        summary="Список постов",
+        description=(
+            "Возвращает список всех постов. "
+            "Доступно без авторизации. Фильтрация по автору "
+            "и дате создания, а также сортировка."
+        ),
+    ),
+    retrieve=extend_schema(
+        summary="Детальная информация о посте",
+        description="Возвращает один пост по его ID. Доступно без авторизации.",
+    ),
+    create=extend_schema(
+        summary="Создание поста",
+        description=(
+            "Создает новый пост. Доступно только авторизованным пользователям. "
+        ),
+    ),
+    update=extend_schema(
+        summary="Полное обновление поста",
+        description="Полностью обновляет пост. Доступно только автору поста.",
+    ),
+    partial_update=extend_schema(
+        summary="Частичное обновление поста",
+        description="Частично обновляет пост. Доступно только автору поста.",
+    ),
+    destroy=extend_schema(
+        summary="Удаление поста",
+        description="Удаляет пост. Доступно только автору поста.",
+    ),
+)
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
